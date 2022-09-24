9eabaa50941c0f5518733b1f95fd163ef4b7cdd6
Fernando-Leon <96993869+Fernando-Leon@users.noreply.github.com>
2022-09-23 21:32:19 -0500

    comind therth

---
+++ JS/main.js
@@ -0,0 +1,25 @@
+const $ = selector => document.querySelector(selector);
+
+const wrapper = $('.wrapper');
+const ticket = $('.ticket');
+
+const { width, height } = wrapper.getBoundingClientRect();
+const halfWidth = width / 2;
+const halfHeight = height / 2;
+
+wrapper.addEventListener('mousemove', event => {
+    ticket.style.transition = 'none';
+
+    const  {offsetX, offsetY} = event;
+    
+    const rotationX = ((offsetX - halfWidth) / halfWidth) * 12;
+    const rotationY = ((offsetY - halfHeight) / halfHeight) * 12;
+
+    ticket.style.transform  = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`
+
+})
+
+wrapper.addEventListener('mouseleave', () => {
+    ticket.style.transition = 'transform .3s ease-in-out';
+    ticket.style.transform = 'rotateX(0deg) rotateY(0deg)';
+})
